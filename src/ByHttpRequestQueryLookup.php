2615cb267a27e1af7a04e212ab9256c849206989
mwjames <jamesin.hongkong.1@gmail.com>
2015-11-23 06:47:35 +0100

    Debug query is not supported

--- src/ByHttpRequestQueryLookup.php
+++ src/ByHttpRequestQueryLookup.php
@@ -8,6 +8,7 @@ use SEQL\ByHttpRequest\QueryResultFetcher;
 use SMW\SQLStore\SQLStore;
 use SMWQuery as Query;
 use SMWQueryResult as QueryResult;
+use Interwiki;
 
 /**
  * @license GNU GPL v2+
@@ -18,28 +19,46 @@ use SMWQueryResult as QueryResult;
 class ByHttpRequestQueryLookup extends SQLStore {
 
 	/**
+	 * @var QueryResultFactory
+	 */
+	private $queryResultFactory;
+
+	/**
 	 * @since 0.1
-	 * 
+	 *
 	 * @param Query $query
 	 *
 	 * @return QueryResult
 	 */
 	public function getQueryResult( Query $query ) {
 
-		$queryResultFactory = new QueryResultFactory( $this );
+		$this->queryResultFactory = new QueryResultFactory( $this );
 
-		$querySource = $query->getQuerySource();
-		$interwiki = \Interwiki::fetch( $querySource );
+		if ( $query->querymode === Query::MODE_DEBUG ) {
+			$query->addErrors( array( wfMessage( 'seql-debug-query-not-supported' )->text() ) );
+			return $this->queryResultFactory->newEmptyQueryResult( $query );
+		}
+
+		$interwiki = $this->tryToMatchInterwikiFor( $query );
 
 		if ( $interwiki === false || $interwiki === null ) {
-			$query->addErrors( array( wfMessage( 'seql-interwiki-prefix-is-missing', $querySource )->text() ) );
-			return $queryResultFactory->newEmptyQueryResult( $query );
+			$query->addErrors( array( wfMessage( 'seql-interwiki-prefix-is-missing', $query->getQuerySource() )->text() ) );
+			return $this->queryResultFactory->newEmptyQueryResult( $query );
 		}
+
+		return $this->fetchQueryResultFor( $query, $interwiki );
+	}
+
+	protected function tryToMatchInterwikiFor( Query $query ) {
+		return Interwiki::fetch( $query->getQuerySource() );
+	}
+
+	protected function fetchQueryResultFor( Query $query, $interwiki ) {
 
 		$queryResultFetcher = new QueryResultFetcher(
 			new HttpRequestFactory(),
-			$queryResultFactory,
-			new JsonResponseParser( new DataValueDeserializer( $querySource ) )
+			$this->queryResultFactory,
+			new JsonResponseParser( new DataValueDeserializer( $query->getQuerySource() ) )
 		);
 
 		$queryResultFetcher->setHttpRequestEndpoint( $interwiki->getApi() );
