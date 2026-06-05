9a3c4b88887c301bd75b0853412c36cb21d91e1d
paladox <paladox@users.noreply.github.com>
2026-06-05 17:43:46 +0100

    Update DataValueDeserializer.php

--- src/DataValueDeserializer.php
+++ src/DataValueDeserializer.php
@@ -2,6 +2,7 @@
 
 namespace SEQL;
 
+use MediaWiki\Title\Title;
 use SMW\DataModel\ContainerSemanticData;
 use SMW\DataValueFactory;
 use SMW\DIProperty;
@@ -97,7 +98,7 @@ class DataValueDeserializer {
 			$value['fulltext'] = substr( $value['fulltext'], ( $pos = strpos( $value['fulltext'], ':' ) ) !== false ? $pos + 1 : 0 );
 		}
 
-		$title = \Title::newFromText( $this->querySource . ':' . str_replace( " ", "_", $value['fulltext'] ), $ns );
+		$title = Title::newFromText( $this->querySource . ':' . str_replace( " ", "_", $value['fulltext'] ), $ns );
 
 		return DIWikiPage::newFromTitle( $title );
 	}
